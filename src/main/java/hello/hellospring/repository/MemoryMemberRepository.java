dc35c780e4a08675bc80f267c1c36a4674d6633f
leeheeweon <ggoddii@kaoni.com>
2021-11-23 08:04:28 +0900

    스프링 ~회원서비스 테스트

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -31,4 +31,7 @@ public class MemoryMemberRepository implements MemberRepository {
         return new ArrayList<>(store.values());
     }
 
+    public void clearStore() {
+        store.clear();
+    }
 }
