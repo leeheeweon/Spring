757c64c3e382b47319f1ef361950f299faf1a805
leeheeweon <ggoddii@kaoni.com>
2021-12-14 08:28:43 +0900

    ~다양한 의존관계 주입 방법

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -18,6 +18,7 @@ public class OrderServiceImpl implements OrderService {
         this.discountPolicy = discountPolicy;
     }
 
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
