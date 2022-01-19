3785c6d9ed98583e9e53bf5f1ec4f48842e27e23
leeheeweon <ggoddii@kaoni.com>
2022-01-20 08:02:50 +0900

    스프링 MVC 2편 -프로젝트생성

---
+++ tymeleaf-basic/tymeleaf-basic/src/test/java/com/hello/tymeleaf/TymeleafBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.tymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TymeleafBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
