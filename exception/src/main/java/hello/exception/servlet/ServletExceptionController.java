aac659630d0997402b8c91b60d524d481b06ed0f
leeheeweon <ggoddii@naver.com>
2022-02-16 08:12:05 +0900

    예외 처리와 오류 페이지 완료

--- exception/src/main/java/hello/exception/servlet/ServletExceptionController.java
+++ exception/src/main/java/hello/exception/servlet/ServletExceptionController.java
@@ -16,6 +16,11 @@ public class ServletExceptionController {
         throw new RuntimeException("예외 발생");
     }
 
+    @GetMapping("/error-400")
+    public void error400(HttpServletResponse response) throws IOException {
+        response.sendError(400, "400페이지를 찾을 수 없습니다.");
+    }
+
     @GetMapping("/error-404")
     public void error404(HttpServletResponse response) throws IOException {
         response.sendError(404, "404페이지를 찾을 수 없습니다.");
