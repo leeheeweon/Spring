efdde674ae51115418cd6bf7c939638a6b278cdd
leeheeweon <ggoddii@naver.com>
2022-05-15 16:03:57 +0900

    family day

--- jdbc/src/main/java/hello/jdbc/JdbcApplication.java
+++ jdbc/src/main/java/hello/jdbc/JdbcApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JdbcApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(JdbcApplication.class, args);
+
     }
 }
