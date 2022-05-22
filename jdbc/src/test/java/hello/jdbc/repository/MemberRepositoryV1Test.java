7e6066e61a66a8b8a2428bb181215d34d1d5c612
leeheeweon <ggoddii@naver.com>
2022-05-23 07:57:24 +0900

    DB락 개념이해 , 조회 , 변경

--- jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
@@ -24,7 +24,6 @@ class MemberRepositoryV1Test {
 
     @BeforeEach
     void beforeEach() {
-
         //기본 DriverManager - 항상 새로운 커넥션을 획득
 //        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
 
