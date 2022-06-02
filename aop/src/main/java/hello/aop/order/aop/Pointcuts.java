5f7c6e485b8cbe50a284a180e5dc0873612e5fa5
leeheeweon <ggoddii@kaoni.com>
2022-06-03 07:47:41 +0900

    fire friday

--- aop/src/main/java/hello/aop/order/aop/Pointcuts.java
+++ aop/src/main/java/hello/aop/order/aop/Pointcuts.java
@@ -3,6 +3,7 @@ package hello.aop.order.aop;
 import org.aspectj.lang.annotation.Pointcut;
 
 public class Pointcuts {
+
     //hello.aop.order 패키지와 하위 패키지
     @Pointcut("execution(* hello.aop.order..*(..))")
     public void allOrder() {
